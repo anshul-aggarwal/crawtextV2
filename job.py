d9f3b1f49851ff3e75914c4ec0c0198561b0624a
C2_4b <4barbes@gmail.com>
2014-09-14 13:32:18 +0200

    Report is ok, user on the go

--- job.py
+++ job.py
@@ -13,14 +13,16 @@ from utils.url import *
 from query import Query
 from scrapper.article import Article		
 from utils import *
+import subprocess
 
 class Job(object):
 	
 	__DB__ = Database(TASK_MANAGER_NAME)
 	__COLL__ = __DB__.use_coll(TASK_COLL)
 	
-	def __init__(self, name, doc): 
-		self.name = name
+	def __init__(self, doc): 
+		
+		self.name = doc['name']
 		self.action = doc["action"]
 		self.__data__ = self.__COLL__.find_one({"name":self.name})		
 		self.project_name = re.sub('[^0-9a-zA-Z]+', '_', self.name)
@@ -29,14 +31,18 @@ class Job(object):
 		
 		#value from db
 		self.__db__ = Database(self.project_name)
+		self.__db__.create_colls(["results","sources", "logs", "queue", "treated"])
 		self.active = True
 		self.date = dt.now()
 		self._doc = doc
 		self.logs = {}
 		self.logs["date"] = self.date
-		self.logs["active"] = True
+		self.active = True
 		
 	def __update_logs__(self):
+		if self.action == "job":
+			self.__COLL__.update({"_id":self.__data__["_id"]}, {"$set":{"action":"crawl"}})
+		self.__COLL__.update({"_id":self.__data__["_id"]}, {"$set":{"active":self.active}})
 		self.__COLL__.update({"_id":self.__data__["_id"]}, {"$push":{"status":self.logs}})
 		print self.logs["msg"]
 		return self.logs["status"]
@@ -64,8 +70,6 @@ class Job(object):
 					print "Be careful, crawl jobs needs more parameters to be active such as query and at least one source url. Check documentation on how to proceed and update this current job"
 			else:
 				self.logs["msg"] = "Successfully created new '%s' job  for project '%s' with parameters:" %(self.action, self.name, ", ".join(added_values))
-			
-			
 			return self.__update_logs__()
 		
 	def update(self):
@@ -151,18 +155,7 @@ class Job(object):
 		self.__COLL__.update({"name":self.name}, {"$set": {"active": "False"}})	
 		return self.__COLL__.update({"name":self.name, "action":self.action}, {"$push": {"status": self.logs}})	
 	
-	def debug(self):
-		print "\n===================="
-		print "DEBUG:", (self.name.upper())
-		print "===================="
-		
-		for job in self.__COLL__.find({"name": self.name}):
-			print "Job is still active?", job["active"]
-			status = job['status']
-			for i, row in enumerate(status):
-				print i, ",".join([row['step'], row['msg'], str(row['status'])])
-		return
-		
+	
 	def delete(self):
 		'''delete project and archive results'''
 		if self.__data__ is None:
@@ -198,27 +191,26 @@ class Job(object):
 		#self.__COLL__.update({"name":self.name}, {"$set": {"active": "False"}})	
 		return self.logs["status"]
 	
-	def show(self, debug=False):
-		if debug:
-			self.debug()
-		else:		
-			print "\n===================="
-			print (self.name.upper())
-			print "===================="
-			for job in self.__COLL__.find({"name": self.name}):
-				
-				print "Job: ", job["action"]
-				print "--------------"
-				for k,v in job.items():
-					if k == '_id' or k == 'status':
-						continue
-					if v is not False or v is not None:
-						print k+":", v			
-				print "--------------"
+	def show(self):
+		
+		print "\n===================="
+		print (self.name.upper())
+		print "===================="
+		print "%i projects active" %(self.__COLL__.find({"name": self.name, "status":"active"}).count())
+		for job in self.__COLL__.find({"name": self.name}):
 			
-			print "____________________\n"
-			return 
-			pass
+			print "Job: ", job["action"]
+			print "--------------"
+			for k,v in job.items():
+				if k == '_id' or k == 'status':
+					continue
+				if v is not False or v is not None:
+					print k+":", v			
+			print "--------------"
+		
+		print "____________________\n"
+		return 
+		pass
 	
 	def __repr__(self):
 		'''representing public info'''
@@ -557,15 +549,18 @@ class Archive(Job):
 		#~ #super(Job, schedule)
 		#~ print "archive"
 		#~ pass
+	def config(self):
+		if self.__db__.queue.count() > 0:
+			self.__db__.queue.drop()
+		self.__db__.queue.insert({"url":self.name, "depth":0})
+		return 
 	def start(self):
-		self.url = self.name
-		
-		self.db.queue.insert({"url":url, "step":0})
-		for doc in self.db.queue.find():
-			if doc['url'] not in self.db.treated.find({"url":doc["url"]}):
-				doc['status'], doc['status_code'], doc['error_type'], doc['url'] = check_url(doc[url])
+		self.config()
+		for doc in self.__db__.queue.find():
+			if doc['url'] not in self.__db__.treated.find({"url":doc["url"]}):
+				doc['status'], doc['status_code'], doc['error_type'], doc['url'] = check_url(doc['url'])
 				if doc['status'] is False:
-					self.db.logs.insert(doc)
+					self.__db__.logs.insert(doc)
 				else:
 					page = Page(doc["url"],doc["depth"])
 					if page.check() and page.request() and page.control():
@@ -573,52 +568,61 @@ class Archive(Job):
 						if article.get() is True:
 							print article.links
 						else:
-							self.db.logs.insert(article.__repr__())
+							self.__db__.logs.insert(article.__repr__())
 					else:
-						self.db.logs.insert(page.__repr__())
-							
-			self.db.treated.insert(doc)
-		#put url in queue
-		#page.extract()
-		print ("Archiving %s") %self.name
+						self.__db__.logs.insert(page.__repr__())
+			
+			self.__db__.treated.insert(doc)
+			self.__db__.queue.remove({"url": doc["url"]})				
+			#put url in queue
+			#page.extract()
+		#~ except Exception as e:
+			#~ print "Error in config %s" %e
+			#~ self.delete()
 		return True
 
-class Export(object):
-	#~ def __init__(self, name, form=None, coll_type=None):
-		#~ 
-		#~ date = datetime.today()
-		#~ self.date = date.strftime('%d-%m-%Y')
-		#~ self.form = form
-		#~ if self.form is None:
-			#~ self.form = "json"
-		#~ 
-		#~ 
-		#~ self.name = name
-		#~ self.coll_type = coll_type
-		#~ self.dict_values = {}
-		#~ self.dict_values["sources"] = {
-							#~ "filename": "export_%s_sources_%s.%s" %(self.name, self.date, self.form),
-							#~ "format": self.form,
-							#~ "fields": 'url,origin,date.date',
-							#~ }
-		#~ self.dict_values["logs"] = {
-							#~ "filename": "export_%s_logs_%s.%s" %(self.name, self.date, self.form), 
-							#~ "format":self.form,
-							#~ "fields": 'url,code,scope,status,msg',
-							#~ }
-		#~ self.dict_values["results"] = {
-							#~ "filename": "export_%s_results_%s.%s" %(self.name, self.date, self.form), 
-							#~ "format":self.form,
-							#~ "fields": 'url,domain,title,content.content,outlinks.url,crawl_date',
-							#~ }	
-		#~ 
+class Export(Job):
+	def __init__(self,doc):
+		Job.__init__(self, doc)
+		try:
+			self.format = self.format
+		except AttributeError:
+			self.format = "json"
+		try:
+			self.coll_type = self.coll_type
+		except AttributeError:
+			self.coll_type = None
+		
+		self.dict_values = {}
+		self.dict_values["sources"] = {
+							"filename": "%s/export_%s_sources_%s.%s" %(self.project_name, self.name, self.date, self.format),
+							"format": self.format,
+							"fields": 'url,origin,date.date',
+							}
+		self.dict_values["logs"] = {
+							"filename": "%s/export_%s_logs_%s.%s" %(self.project_name,self.name, self.date, self.format), 
+							"format":self.format,
+							"fields": 'url,code,scope,status,msg',
+							}
+		self.dict_values["results"] = {
+							"filename": "%s/export_%s_results_%s.%s" %(self.project_name,self.name, self.date, self.format), 
+							"format":self.format,
+							"fields": 'url,domain,title,content.content,outlinks.url,crawl_date',
+							}	
+							
+	def create(self):
+		if self.coll_type is not None:
+			return self.export_one()
+		else:
+			return self.export_all()
+		
 			
 	def export_all(self):
 		datasets = ['sources', 'results', 'logs']
 		filenames = []
 		for n in datasets:
 			dict_values = self.dict_values[str(n)]
-			if self.form == "csv":
+			if self.format == "csv":
 				print ("- dataset '%s' in csv:") %n
 				c = "mongoexport -d %s -c %s --csv -f %s -o %s"%(self.name,n,dict_values['fields'], dict_values['filename'])	
 				filenames.append(dict_values['filename'])		
@@ -627,13 +631,13 @@ class Export(object):
 				c = "mongoexport -d %s -c %s -o %s"%(self.name,n,dict_values['filename'])				
 				filenames.append(dict_values['filename'])
 			subprocess.call(c.split(" "), stdout=open(os.devnull, 'wb'))
-			#moving into report/name_of_the_project
 			
-			subprocess.call(["mv",dict_values['filename'], self.directory], stdout=open(os.devnull, 'wb'))
+			
+			#subprocess.call(["mv",dict_values['filename'], self.project_name], stdout=open(os.devnull, 'wb'))
 			print ("into file: '%s'") %dict_values['filename']
 		ziper = "zip %s %s_%s.zip" %(" ".join(filenames), self.name, self.date)
 		subprocess.call(ziper.split(" "), stdout=open(os.devnull, 'wb'))
-		return "\nSucessfully exported 3 datasets: %s of project %s into directory %s" %(", ".join(datasets), self.name, self.directory)		
+		return "\nSucessfully exported 3 datasets: %s of project %s into directory %s" %(", ".join(datasets), self.name, self.project_name)		
 	
 	def export_one(self):
 		if self.coll_type is None:
@@ -648,8 +652,8 @@ class Export(object):
 				c = "mongoexport -d %s -c %s --jsonArray -o %s"%(self.name,self.coll_type,dict_values['filename'])				
 			subprocess.call(c.split(" "), stdout=open(os.devnull, 'wb'))
 			#moving into report/name_of_the_project
-			subprocess.call(["mv",dict_values['filename'], self.directory], stdout=open(os.devnull, 'wb'))
-			return "Sucessfully exported %s dataset of project %s into %s/%s" %(str(self.coll_type), str(self.name), self.directory, str(dict_values['filename']))
+			subprocess.call(["mv",dict_values['filename'], self.project_name], stdout=open(os.devnull, 'wb'))
+			return "Sucessfully exported %s dataset of project %s into %s/%s" %(str(self.coll_type), str(self.name), self.project_name, str(dict_values['filename']))
 		except KeyError:
 			return "there is no dataset called %s in your project %s"%(self.coll_type, self.name)
 			
@@ -660,22 +664,24 @@ class Export(object):
 			return self.export_all()
 			
 					
-class Report(object):
-	def __init__(self, name, format="txt"):
-		date = dt.now()
-		self.name = name
-		self.db = Database(self.name)
-		self.date = date.strftime('%d-%m-%Y_%H-%M')
-		self.format = format
-	
+class Report(Job):
+	#~ def __init__(self, name, format="txt"):
+		#~ date = dt.now()
+		#~ self.name = name
+		#~ self.db = Database(self.name)
+		#~ self.date = date.strftime('%d-%m-%Y_%H-%M')
+		#~ self.format = format
+	def create(self):
+		return self.txt_report()
+		
 	def txt_report(self):
-		name = re.sub("[^0-9a-zA-Z]","_", self.name)
-		self.directory = "%s/reports" %name
+		self.report_date = self.date.strftime('%d-%m-%Y_%H-%M')
+		self.directory = "%s/reports" %self.project_name
 		if not os.path.exists(self.directory):
 			os.makedirs(self.directory)
-		filename = "%s/Report_%s_%s.txt" %(self.directory, self.name, self.date)
+		filename = "%s/%s.txt" %(self.directory, self.report_date)
 		with open(filename, 'a') as f:
-			f.write(self.db.stats())
+			f.write(self.__db__.stats())
 		print ("Successfully generated report for %s\nReport name is: %s") %(self.name, filename)
 		return True
 	
@@ -683,14 +689,79 @@ class Report(object):
 		pass
 	def email_report(self):
 		pass			
-	def run_job(self):
-		if self.format == "txt":
-			return self.txt_report()
-		else:
-			raise NotImplemented
+	def start(self):
+		return self.txt_report()
+		
 			
 class User(Job):
-	pass
-
-
-	
+	def show(self):
+		print "\n===================="
+		print "USER:", (self.name.upper())
+		print "===================="
+		print "%i projects active" %(self.__COLL__.find({"user": self.name, "status":"active"}).count())
+		if self.__COLL__.find({"user": self.name}) is None:
+			print "User not registrated"
+			return 
+		for job in self.__COLL__.find({"user": self.name}):
+			
+			print "Job: ", job["action"]
+			print "--------------"
+			for k,v in job.items():
+				if k == '_id' or k == 'status':
+					continue
+				if v is not False or v is not None:
+					print k+":", v			
+			print "--------------"
+		
+		print "____________________\n"
+		return 
+		
+	def create(self):
+		self.logs["step"] = "registering new user"
+		self.logs["status"] = True
+		self.user = self.name
+		self.name = "none"
+		self.action = "crawl"
+		self.active = False
+		j = Job(self.__dict__)
+		return j.create()
+	def delete(self):
+		print self.name
+		jobs = self.__COLL__.find({"user": self.name})
+		if jobs is None:
+			"No project found with user %s", self.name
+		else:
+			for job in jobs:
+				print job
+				j = Job(job)
+				print j.delete()
+		
+	def unschedule(self):
+		self.logs["step"] = "unscheduling user"
+		self.logs["status"] = True
+		
+		if self.__COLL__.find({"user": self.name}) is None:
+			self.logs["msg"] = "User not registrated"
+			self.logs["status"] = True
+			return 
+		else:
+			for job in self.__COLL__.find({"user": self.name}):
+				print job["user"], job["name"]
+				#print job.upsert({"_id": job['_id']}, {"$unset":{"user": self.name}})
+				print self.__COLL__.insert({"_id": job['_id']}, {"$unset":{"user": self.name}})
+			self.logs["msg"] = "User successfully unregistered from project"
+		print self.logs["msg"]
+		return self.logs
+		
+class Debug(Job):
+	def start(self):
+		print "\n===================="
+		print "DEBUG:", (self.name.upper())
+		print "===================="
+		for job in self.__COLL__.find({"name": self.name}):
+			print "Job is still active?", job["active"]
+			status = job['status']
+			for i, row in enumerate(status):
+				print i, ",".join([row['step'], row['msg'], str(row['status'])])
+		return
+			
--- wk.py
+++ wk.py
@@ -13,10 +13,11 @@ class Worker(object):
 	''' main access to Job Database'''
 	__DB__ = Database(TASK_MANAGER_NAME)
 	__COLL__ = __DB__.use_coll(TASK_COLL)
-	ACTION_LIST = ["report", "extract", "export", "archive", "start","stop", "delete","list", 'schedule', "unschedule", "debug"]
-	PROJECT_LIST = ["--user", "--repeat"]
-	CRAWL_LIST = ["--query", "--key"]
-	OPTION_lIST	= ['add', 'delete', 'expand']
+	_ACTION_LIST = ["report", "extract", "export", "archive", "user", "debug", "wos"]
+	_TASK_LIST = ["start","stop", "delete","list", 'schedule', "unschedule"]
+	_PROJECT_LIST = ["--user", "--repeat"]
+	_CRAWL_LIST = ["--query", "--key"]
+	_OPTION_lIST	= ['add', 'delete', 'expand']
 	
 	
 	def __init__(self,user_input):
@@ -34,51 +35,60 @@ class Worker(object):
 		'''mapping user input into job parameters'''
 		
 		self.name = user_input['<name>']
-		self._task = None
-		self.action = None
+		self._task = [k for k,v in user_input.items() if v is True and k in self._TASK_LIST]
+		self.action = [k for k,v in user_input.items() if v is True and k in self._ACTION_LIST]
+		if len(self._task) == 0:
+			self._task = None
+		else:
+			self._task = self._task[0]
+		if len(self.action) == 0:
+			self.action  = None
+		else:
+			self.action = self.action[0]
+		
 		#user
 		if validate_email(self.name) is True:
 			self.user = self.name
+			self._task = self.action
 			self.action = "user"
-			self._task = "show"
-			return self
-		else:
+			
+		#archive
+		elif validate_url(self.name) is True:
+			self.url = self.name
+			self._task = self.action
+			self.action = "archive"
+			
+		#crawl update	
+		elif user_input["-s"] is True:
+			self.action = "crawl"
 			for k,v in user_input.items():
-				if v is True and k in self.ACTION_LIST:
-					self.action = "job"
-					self._task = k
-				if v is not None and k in self.CRAWL_LIST:	
-					self.action = "crawl"
-					self._task ="update"
-				if v is not None and k in self.PROJECT_LIST:	
+				if v is True and k in self._OPTION_lIST:
+					self._task = k+"_sources"
+				if v is not None and v is not False:
+					setattr(self,re.sub("<|>|--","", k), v)
+		else:
+			if self.action is None:
+				self.action = "crawl"
+				for k,v in user_input.items():
+					if v is not None and k in self._CRAWL_LIST:	
+						self._task ="update"
+				if v is not None and k in self._PROJECT_LIST:	
 					self.action = "job"
 					self._task = "update"
 				if v is not None and v is not False and k != "<name>":
 					setattr(self, re.sub("--|<|>","", k), v)
-			
-			#archive	
-			if validate_url(self.name) is True:
-				self.action = "archive"
-				try:
-					self.format = self.format
-					self._task = "update"
-				except AttributeError:
-					pass
-				return self
-			elif user_input["-s"] is True:
-				self.action = "crawl"
-				
-				for k,v in user_input.items():
-					if v is True and k in self.OPTION_lIST:
-						self._task = k+"_sources"
-					if v is not None and v is not False:
-						setattr(self,re.sub("<|>","", k), v)
-				return self
 			else:
-				return self
-				
+				print self.action
+		for k,v in user_input.items():
+			if v is not None and v is not False:
+				setattr(self,re.sub("<|>|--","", k), v)
+		return self					
 	def dispatch(self):
-		item = self.__COLL__.find_one({"name":self.name})
+		print self.action, self._task
+		if self.action == "user":
+			item = self.__COLL__.find_one({"user":self.name})
+		else:	
+			item = self.__COLL__.find_one({"name":self.name})
 		
 		if self._task is None: 
 			if item is not None:
@@ -86,14 +96,12 @@ class Worker(object):
 			else:
 				self._task = "create"
 			
-		if self.action is None or self.action == "job":
-			self.action = "crawl"
 		
 				
 		_class = (self.action).capitalize()
 		instance = globals()[_class]
-		
-		job = instance(self.name, self.__dict__)
+		print instance, self._task
+		job = instance(self.__dict__)
 		
 		instanciate = getattr(job,self._task)
 		'''
@@ -106,23 +114,4 @@ class Worker(object):
 		
 		return instanciate()
 		
-			
-	
-	def export_job(self):	
-		#next change self.action
-		self.logs["step"] = "Export"
-		self.logs["msg"] = "Exporting %s" %(self.name)
-		self.__select_jobs__({"name":self.name, "action":"crawl"})
-		if self.job_list is None:
-			print "No active crawl job found for %s. Export can be executed" %self.name
-			return
-			#return self.__COLL__.update({"name":self.name, "action":self.action}, {"$push": {"status": self.logs}})
-		else:
-			job = Export(self.name)
-			job.run_job()
-			self.__COLL__.update({"name":self.name, "action":self.action}, {"$push": {"status": job.logs}})
-			#self.status = e.status
-			return self.__COLL__.update({"name":self.name, "action":self.action}, {"$push": {"status": self.logs}})
-			
-				
 		
